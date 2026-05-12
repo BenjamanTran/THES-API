1e7c8506fcdae8b193534632c74997f9f8f5a2da
BenjamanTran <t16021999@gmail.com>
2026-05-12 17:12:43 +0700

    fix bug deploy cloudrun

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [:id]
 
+  # Use Redis for Action Cable instead of Solid Cable
+  config.action_cable.adapter = :redis
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
